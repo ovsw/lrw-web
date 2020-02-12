fe1915f011bf4391831cbb5da803641b2b0d622a
Ovi Savescu <ovi@ovswebsites.com>
2020-02-12 14:13:54 +0200

    added support for cognito forms optional responsive script insertion

--- src/components/serializers/Embed.js
+++ src/components/serializers/Embed.js
@@ -1,3 +1,13 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
-export default ({node}) => <div dangerouslySetInnerHTML={{__html: node.code}} />
+export default ({node}) => (
+  <>
+    {node.cognitoForm && (
+      <Helmet>
+        <script src='https://services.cognitoforms.com/scripts/embed.js' />
+      </Helmet>
+    )}
+    <div dangerouslySetInnerHTML={{__html: node.code}} />
+  </>
+)
