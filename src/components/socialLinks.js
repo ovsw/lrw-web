50115f59fd2da39c7dae1201715292303a84ce4a
Ovi Savescu <ovi@ovswebsites.com>
2020-02-17 15:46:52 +0200

    social links updated

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -2,7 +2,7 @@
 import React from 'react' // eslint-disable-line
 import {jsx} from 'theme-ui'
 
-import {FaFacebookSquare, FaYoutubeSquare, FaTwitterSquare} from 'react-icons/fa'
+import {FaFacebookSquare, FaYoutubeSquare, FaTwitterSquare, FaInstagram} from 'react-icons/fa'
 
 const SocialLinks = () => {
   return (
@@ -22,9 +22,10 @@ const SocialLinks = () => {
         }
       }
     }}>
-      <li><a href='https://facebook.com' sx={{color: '#3b5998'}} rel='noopener noreferrer'><FaFacebookSquare /></a></li>
-      <li><a href='https://youtube.com' sx={{color: '#ff0000'}} rel='noopener noreferrer'><FaYoutubeSquare /></a></li>
-      <li><a href='https://twitter.com' sx={{color: '#00acee'}} rel='noopener noreferrer'><FaTwitterSquare /></a></li>
+      <li><a href='https://www.instagram.com/camplaurelwood/' sx={{color: '#8134AF'}} rel='noopener noreferrer'><FaInstagram /></a></li>
+      <li><a href='https://www.facebook.com/CampLaurelwood/' sx={{color: '#3b5998'}} rel='noopener noreferrer'><FaFacebookSquare /></a></li>
+      <li><a href='https://www.youtube.com/channel/UC_Kr62M5Wdnoe6U3YLXoFhg' sx={{color: '#ff0000'}} rel='noopener noreferrer'><FaYoutubeSquare /></a></li>
+      <li><a href='https://twitter.com/camp_laurelwood' sx={{color: '#00acee'}} rel='noopener noreferrer'><FaTwitterSquare /></a></li>
     </ul>
   )
 }
