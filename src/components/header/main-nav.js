3ba4d4318a165efe25d365d79b0420a0e30bf464
Ovi Savescu <ovi@ovswebsites.com>
2020-02-23 07:37:47 +0200

    improved formatting

--- src/components/header/main-nav.js
+++ src/components/header/main-nav.js
@@ -21,34 +21,24 @@ const MainNav = ({navStructure, showNav, onHideNav}) => {
     }
   }
 `)
-
+  const navMobile = {
+    bg: 'primary',
+    maxHeight: showNav ? '1000px' : '0',
+    transition: 'all 400ms ease-out',
+    position: ['absolute', 'absolute'],
+    width: 'auto',
+    height: ['88vh', '90vh'],
+    overflow: 'scroll',
+    top: ['5.15rem', '5.15rem'],
+    left: 0,
+    right: 0,
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.25)'
+  }
   return (
     <>
       {/* mobile */}
-      <nav
-        sx={{
-          bg: 'primary',
-          maxHeight: showNav ? '1000px' : '0',
-          transition: 'all 400ms ease-out',
-          position: ['absolute', 'absolute'],
-          width: 'auto',
-          height: ['88vh', '90vh'],
-          overflow: 'scroll',
-          top: ['5.15rem', '5.15rem'],
-          left: 0,
-          right: 0,
-          boxShadow: '0 0 10px rgba(0, 0, 0, 0.25)'
-        }}
-      >
-        <Flex as='ul' sx={{
-          variant: 'lists.reset',
-          m: 0,
-          pt: [0, 0],
-          pb: [0, 4],
-          px: 0,
-          display: ['block', 'block', 'flex'],
-          justifyContent: 'flex-end'
-        }}>
+      <nav sx={navMobile}>
+        <Flex as='ul' sx={navMobileUl}>
           {navStructure.map((mainMenuItem, i) => {
             return (
               <MobileMenu key={mainMenuItem.slug} menuTitle={mainMenuItem.title} mainLink={mainMenuItem.slug} menuColumns={mainMenuItem.children} onHideNav={onHideNav} />
@@ -65,37 +55,15 @@ const MainNav = ({navStructure, showNav, onHideNav}) => {
           width: 'full'
         }}
       >
-        <Flex as='ul' sx={{
-          variant: 'lists.reset',
-          m: 0,
-          pt: [3, 3, 1],
-          pb: [4, 4, 1],
-          px: [0, 0, 0, 2, 3],
-          display: ['block', 'block', 'flex'],
-          justifyContent: 'space-around'
-        }}>
+        <Flex as='ul' sx={navUlDesk}>
           {navStructure.map((mainMenuItem, i) => {
             if (i > 3) { return } // only show the first 4 items in the desktop menu
             if (i === 2) {
               return (
                 <React.Fragment key={mainMenuItem.slug}>
-                  <li sx={{
-                    flexGrow: '1',
-                    position: 'relative',
-                    display: ['none', 'none', 'block']
-                  }}>
-                    <Link to='/' sx={{
-                      display: 'block',
-                      position: 'absolute',
-                      width: '100%',
-                      left: '100px',
-                      marginLeft: '-100px',
-                      top: '-45px',
-                      textAlign: 'center'
-                    }}>
-                      <Img fixed={logoImage.childImageSharp.fixed} fadeIn={false} loading='eager' sx={{
-                        width: '120px', position: 'relative', top: '-12px'
-                      }} alt='Camp Laurelwood Logo' />
+                  <li sx={navUlDesk.navLiItem}>
+                    <Link to='/' sx={logoLink}>
+                      <Img sx={logoLink.Img} fixed={logoImage.childImageSharp.fixed} fadeIn={false} loading='eager' alt='Camp Laurelwood Logo' />
                     </Link></li>
                   <MegaMenu menuTitle={mainMenuItem.title} mainLink={mainMenuItem.slug} menuColumns={mainMenuItem.children} key={mainMenuItem.slug} />
                 </React.Fragment>
@@ -111,6 +79,48 @@ const MainNav = ({navStructure, showNav, onHideNav}) => {
       </nav>
     </>
   )
+}
+
+// mobile styles
+const navMobileUl = {
+  variant: 'lists.reset',
+  m: 0,
+  pt: [0, 0],
+  pb: [0, 4],
+  px: 0,
+  display: ['block', 'block', 'flex'],
+  justifyContent: 'flex-end'
+}
+
+//  desktop styles
+const navUlDesk = {
+  variant: 'lists.reset',
+  m: 0,
+  pt: [3, 3, 1],
+  pb: [4, 4, 1],
+  px: [0, 0, 0, 2, 3],
+  display: ['block', 'block', 'flex'],
+  justifyContent: 'space-around',
+
+  'navLiItem': {
+    flexGrow: '1',
+    position: 'relative',
+    display: ['none', 'none', 'block']
+  }
+}
+const logoLink = {
+  display: 'block',
+  position: 'absolute',
+  width: '100%',
+  left: '100px',
+  marginLeft: '-100px',
+  top: '-45px',
+  textAlign: 'center',
+  'Img': {
+    width: '120px',
+    position: 'relative',
+    top: '-12px'
+  }
 }
 
 export default MainNav
