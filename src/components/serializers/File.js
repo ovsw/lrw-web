e1d45ec28fb25f98fb1100d070fa477b9eed4821
Ovi Savescu <ovi@ovswebsites.com>
2020-02-27 16:27:36 +0200

    file links open in a new tab

--- src/components/serializers/File.js
+++ src/components/serializers/File.js
@@ -6,6 +6,6 @@ import {FaFileAlt} from 'react-icons/fa'
 export default ({node}) => {
   // console.log(node)
   return (
-    <a href={node.asset.url} rel='noopener noreferrer' className='contentButton'><FaFileAlt sx={{position: 'relative', top: '2px'}} />  {node.description}</a>
+    <a href={node.asset.url} rel='noopener noreferrer' className='contentButton' target='_blank'><FaFileAlt sx={{position: 'relative', top: '2px'}} />  {node.description}</a>
   )
 }
