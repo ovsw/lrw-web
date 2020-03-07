035df3f4549f0e9812bf97ba6b7eff22ad12323a
Ovi Savescu <ovi@ovswebsites.com>
2020-03-07 11:55:11 +0200

    removed ref

--- src/components/header2/TopHeader/Column1.js
+++ src/components/header2/TopHeader/Column1.js
@@ -42,7 +42,7 @@ const Column1 = () => {
 
       {/* <div className='enroll-link'>
         <span className='svg-ic'><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' data-name='Layer 1' viewBox='0 0 18.57 18.59'><title>SDC_Icons_Final_JV</title><polygon points='13.88 17.69 0.9 17.69 0.9 4.7 6.17 4.7 6.17 3.8 0 3.8 0 18.59 14.78 18.59 14.78 11.6 13.88 11.6 13.88 17.69' /><path d='M4.2,14.41,9,13.63l9.52-9.55L14.48,0,5,9.56ZM14.48,1.27,17.3,4.08l-1.59,1.6L12.89,2.86ZM5.8,10,12.26,3.5l2.81,2.81L8.62,12.79l-3.34.53Z' /></svg></span>
-        <a href='https://shibley.campintouch.com/ui/forms/application/camper/App' title='Enroll Now'>Enroll Now</a>
+        <a href='/' title='Enroll Now'>Enroll Now</a>
       </div> */}
     </div>
   )
